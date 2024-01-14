b11c0b45fb49d6aa68ce1fb2d6f1342fe20bae40
yan <eugenescat@gmail.com>
2024-01-13 22:43:56 -0500

    Injection's fundamental class

---
+++ src/main/java/com/minis/beans/PropertyValue.java
@@ -0,0 +1,24 @@
+package com.minis.beans;
+
+public class PropertyValue {
+  private final String name;
+  private final Object value;
+  private final String type;
+  public PropertyValue(String type, String name, Object value) {
+    this.type = type;
+    this.name = name;
+    this.value = value;
+  }
+
+  public String getType() {
+    return this.type;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public Object getValue() {
+    return this.value;
+  }
+}
